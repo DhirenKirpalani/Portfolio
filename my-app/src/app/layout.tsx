1ff6afa92eb80dbf48187d3ed95cc5a198df46e9
DhirenKirpalani <dhirenkirpalani2308@gmail.com>
2024-05-04 12:39:43 +0700

    chore: update metadata

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -20,8 +20,16 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  
   return (
     <html lang="en" className="!scroll-smooth">
+      <head>
+        <title>Dhiren | Personal Portfolio</title>
+        <meta name="description" content="Dhiren Kirpalani is a fresh graduate in full stack development." />
+        <meta property="og:title" content="Dhiren | Personal Portfolio" />
+        <meta property="og:description" content="Dhiren Kirpalani is a fresh graduate in full stack development." />
+        <meta property="og:image" content="/preview.png" />
+      </head>
       <body className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36
       dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}>
         <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] 
